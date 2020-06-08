0bd4dcc5070e6681db15f90d428c0936819c8bb0
Joe Palala <jose.palala@live.com>
2020-06-08 18:32:50 +0800

    add base components and ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
